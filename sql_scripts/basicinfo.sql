54960abc67343b50a18266cca126ca93204a6f09
Sam Hill <soshjam@soshjam.com>
2025-03-30 12:03:20 -0600

    Development database setup scripts

---
+++ sql_scripts/basicinfo.sql
@@ -0,0 +1,27 @@
+INSERT INTO users VALUES (1, "john_doe", "john@example.com", "hashed_password1", NULL);
+INSERT INTO users VALUES (2, "jane_doe", "jane@example.com", "hashed_password2", NULL);
+
+INSERT INTO boards VALUES (1, "main_board", "default", 1, true);
+INSERT INTO boards VALUES (2, "child_board", "default", 1, false);
+INSERT INTO boards VALUES (3, "other_board", "default", 2, true);
+
+INSERT INTO editor_table VALUES (1, 3);
+
+INSERT INTO items VALUES (1, 1, "0,0", null, null, "note");
+INSERT INTO items_note VALUES (1, "Example note", "300,200");
+
+INSERT INTO items VALUES (2, 1, "0,250", null, null, "link");
+INSERT INTO items_link VALUES (2, "Link to Child Board", "/boards/2");
+
+INSERT INTO items VALUES (3, 1, "350,0", null, null, "list");
+INSERT INTO items_list VALUES (3, "Example List");
+INSERT INTO items VALUES (4, 1, null, 3, 0, "note");
+INSERT INTO items_note VALUES (4, "List Item 1", "300,200");
+INSERT INTO items VALUES (5, 1, null, 3, 1, "note");
+INSERT INTO items_note VALUES (5, "List Item 2", "300,200");
+
+INSERT INTO items VALUES (6, 1, "700,0", null, null, "todo");
+INSERT INTO items_todo VALUES (6, "Todo List");
+INSERT INTO todo_items VALUES (1, 6, "Plan API", null, true);
+INSERT INTO todo_items VALUES (2, 6, "Read Other Board", "/boards/3", false);
+INSERT INTO todo_items VALUES (3, 6, "Finish API", null, false);
---
+++ sql_scripts/cleartables.sql
@@ -0,0 +1,15 @@
+DELETE FROM todo_items;
+
+DELETE FROM items_note;
+DELETE FROM items_link;
+DELETE FROM items_media;
+DELETE FROM items_todo;
+DELETE FROM items WHERE items.list_id IS NOT NULL;
+DELETE FROM items_list;
+DELETE FROM items;
+
+DELETE FROM editor_table;
+DELETE FROM refresh_tokens;
+DELETE FROM images;
+DELETE FROM boards;
+DELETE FROM users;

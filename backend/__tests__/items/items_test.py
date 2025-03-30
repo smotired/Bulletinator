5faeb6abd526b33ec37b66aa5dd108f7dabba879
Sam Hill <soshjam@soshjam.com>
2025-03-30 16:34:12 -0600

    tests for get item

--- backend/__tests__/items/items_test.py
+++ backend/__tests__/items/items_test.py
@@ -17,7 +17,7 @@ def test_get_items2(client, get_item):
     assert response.status_code == 200
 
 def test_get_private_items(client, auth_headers, get_item):
-    response = client.get("/boards/3/items", headers=auth_headers(2))
+    response = client.get("/boards/3/items", headers=auth_headers(3))
     assert response.json() == {
         "metadata": { "count": 1 },
         "items": [ get_item(8) ]
@@ -28,3 +28,43 @@ def test_get_private_items_unauthorized(client, auth_headers, exception):
     response = client.get("/boards/3/items", headers=auth_headers(4)) # user 4 should have no knowledge of board 3
     assert response.json() == exception("entity_not_found", "Unable to find board with id=3")
     assert response.status_code == 404
+
+def test_get_item(client, get_item):
+    response = client.get("/boards/1/items/1")
+    assert response.json() == get_item(1)
+    assert response.status_code == 200
+
+def test_get_404_item(client, exception):
+    response = client.get("/boards/1/items/404")
+    assert response.json() == exception("entity_not_found", "Unable to find item with id=404")
+    assert response.status_code == 404
+
+def test_get_private_item(client, auth_headers, get_item):
+    response = client.get("/boards/3/items/8", headers=auth_headers(3))
+    assert response.json() == get_item(8)
+    assert response.status_code == 200
+
+def test_get_private_item_unauthorized(client, auth_headers, exception):
+    response = client.get("/boards/3/items/8", headers=auth_headers(4))
+    assert response.json() == exception("entity_not_found", "Unable to find board with id=3")
+    assert response.status_code == 404
+
+def test_get_item_on_different_board(client, exception):
+    response = client.get("/boards/2/items/1")
+    assert response.json() == exception("entity_not_found", "Unable to find item with id=1")
+    assert response.status_code == 404
+
+def test_get_item_link(client, get_item):
+    response = client.get("/boards/1/items/1")
+    assert response.json() == get_item(1)
+    assert response.status_code == 200
+
+def test_get_item_todo(client, get_item):
+    response = client.get("/boards/1/items/5")
+    assert response.json() == get_item(5)
+    assert response.status_code == 200
+
+def test_get_item_list(client, get_item):
+    response = client.get("/boards/2/items/2")
+    assert response.json() == get_item(2)
+    assert response.status_code == 200

7bde150a6284bea192652e80da71f36d8a120fe1
Sam Hill <soshjam@soshjam.com>
2025-04-13 01:02:39 -0600

    Finalize email form

--- backend/email.py
+++ backend/email.py
@@ -12,99 +12,52 @@ def send_verification_email(account: DBAccount, verification: DBEmailVerificatio
         port=settings.smtp_port,
     ) as server:
         server.login(settings.smtp_login, settings.smtp_password)
-        server.sendmail(
-            settings.smtp_sender,
-            verification.email,
-            compose_verification_email(account, verification)
-        )
+        server.send_message( compose_verification_email(account, verification) )
 
-def compose_verification_email(account: DBAccount, verification: DBEmailVerification):
-    # Specify HTML 
-    # TODO: load this from a file god damn
-    html = """\
-<html>
-  <style>
-    body {
-      margin: 0;
-      padding: 0;
-      background: #eeeeee;
-      font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    }
+def compose_verification_email(account: DBAccount, verification: DBEmailVerification) -> MIMEMultipart:
+    # Specify plain text
+    text = f"""\
+Welcome, {account.display_name or account.username}!
 
-    main {
-      display: block;
-      width: 60%;
-      margin: 1rem auto;
-      background: #ffffff;
-      box-shadow: 5px 5px 10px #888888;
-      border-radius: 2px;
-    }
+Thanks for creating a Bulletinator account. Verify your email by clicking the link below to start managing projects.
+https://www.bulletinator.com/verify-email/{verification.id}
 
-    main section{
-      padding: 2rem;
-    }
-
-    h1 {
-      text-align: center;
-    }
-
-    p.button {
-      text-align: center;
-      margin-top: 2rem;
-    }
-
-    p.button a {
-      background-color: #262262;
-      background-size: 100% 200%;
-      background-position: 0 100%;
-      background-image: radial-gradient(circle at 72% -71%, #2bb673 26%, transparent 68.05%);
-      color: white;
-      border-radius: 0.5rem;
-      padding: 1rem 2rem;
-      text-decoration: none;
-      transition: all 0.1s ease-in-out;
-      box-shadow: 0px 0px 0px #888888;
-    }
-
-    p.button a:hover {
-      text-decoration: underline;
-      background-color: #264482;
-      background-position: 0% 0%;
-      box-shadow: 2px 2px 8px #888888;
-    }
-
-    p.sm {
-      color: #888888;
-      text-align: center;
-      font-size: 0.75rem;
-    }
-
-    @media(max-width: 50rem) {
-      main {
-        width: calc(100% - 1rem - 3rem);
-      }
-    }
-  </style>
-  <body>
-    <main>
-      <section>
-        <h1>Welcome, {account.display_name or account.username}!</h1>
-        <p>Thanks for creating a Bulletinator account. Verify your email to start managing projects.</p>
-        <p class="button"><a class="button" href="https://www.bulletinator.com/verify-email/{verification.id}" target="_blank" rel="noopener noreferrer">Verify Email</a></p>
-      </section>
-    </main>
-    <p class="sm">Questions? Shoot an email to <a href="mailto:support@bulletinator.com">support@bulletinator.com</a>.</p>
-  </body>
-</html>
+You received this email because you signed up for an account at Bulletinator.com.
+If you do not recognize this action, or you have other questions, please send an email to support@bulletinator.com.
 """
 
+    # Specify HTML 
+    html = f"""\
+<html>
+  <body style="margin: 0; padding: 1rem 0; background-color: #eeeeee; font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;">
+    <div style="width: calc(100% - 3rem); max-width: 40rem; margin: auto; background-color: #ffffff; padding: 2rem 1.5rem;">
+      <img src="https://i.imgur.com/iR9I0NN.png" alt="Bulletinator Logo" style="display: block; margin: auto; max-height:5rem; max-width: 80%;" />
+        <h1 style="text-align: center;">Welcome, {account.display_name or account.username}!</h1>
+        <p style="text-align: center;">Thanks for creating a Bulletinator account. Verify your email to start managing projects.</p>
+        <p style="text-align: center; margin-top: 2rem;">
+          <a href="https://www.bulletinator.com/verify-email/{verification.id}" target="_blank" rel="noopener noreferrer" style="background-color: #262262; color: white; text-decoration: none; border-radius: 0.5rem; padding: 1rem 2rem;">
+            Verify Email
+          </a>
+        </p>
+    </div>
+    <p style="color: #888888; text-align: center; font-size: 0.75rem;">
+      You received this email because you signed up for an account at <a href="https://www.bulletinator.com/" target="_blank" rel="noopener noreferrer">Bulletinator.com</a>.
+    </p>
+    <p style="color: #888888; text-align: center; font-size: 0.75rem;">
+      If you do not recognize this action, or you have other questions, please send an email to <a href="mailto:support@bulletinator.com">support@bulletinator.com</a>.
+    </p>
+  </body>
+</html>
+""" # TODO: Replace image link once frontend is hosted
+
     # Create a multipart message and set headers
-    message = MIMEMultipart()
+    message = MIMEMultipart("alternative")
     message["From"] = settings.smtp_sender
-    message["To"] = settings.smtp_sender
+    message["To"] = verification.email
     message["Subject"] = "Verify your Bulletinator account"
 
-    # Attach the HTML part
+    # Attach different segments
+    message.attach(MIMEText(text, "plain"))
     message.attach(MIMEText(html, "html"))
 
-    return message.as_string()
+    return message
